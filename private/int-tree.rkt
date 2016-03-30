79a6f503933a929f433216661c83d92047d24496
ben <benjaminlgreenman@gmail.com>
2016-03-30 04:02:17 -0400

    [private] copied rest of int-tree

--- private/int-tree.rkt
+++ private/int-tree.rkt
@@ -1,5 +1,16 @@
 #lang racket/base
 
+(provide
+  make-int-tree
+  int-tree-clear
+  int-tree-delete
+  int-tree-insert
+  int-tree-search
+  int-tree-searchGTE
+  int-tree-searchLTE
+)
+
+;; =============================================================================
 
 (define (id x)
   x)
@@ -28,8 +39,63 @@
 (define (int-tree-clear I)
   (set-box! (int-tree-root I) #f))
 
+(define (int-tree-delete I z)
+  (define y
+    (if (or (not (node-left z)) (not (node-right z)))
+      z
+      (node-successor z)))
+  (define x
+    (or (node-left y) (node-right y)))
+  (define yparent (node-parent y))
+  (define yleft? (eq? y (and yparent (node-left yparent))))
+  (define ycolor (node-color y))
+  (when x (set-node-parent! x yparent))
+  (cond
+   [(not yparent)
+    (set-box! (int-tree-root I) x)]
+   [yleft?
+    (set-node-left! yparent x)]
+   [else
+    (set-node-right! yparent x)])
+  (when (not (eq? y z)) (node-replace I z y))
+  (when ycolor
+    (if x
+      (delete-fixup I x)
+      (when yparent
+        (let ([yparent (if (eq? z yparent) y yparent)])
+          (set-node-color! z #t)
+          (set-node-left! z #f)
+          (set-node-right! z #f)
+          (set-node-parent! z yparent)
+          (if yleft?
+            (set-node-left! yparent z)
+            (set-node-right! yparent z))
+          (delete-fixup I x)
+          (if (eq? z (node-left (node-parent z)))
+            (set-node-left! (node-parent z) #f)
+            (set-node-right! (node-parent z) #f))))))
+  (node-free! z))
+
 (define (int-tree-insert I z)
-  'todo)
+  (define z-key (node-key z))
+  (define y
+    (let loop ([x (unbox (int-tree-root I))] [prev #f])
+      (if (not x)
+        prev
+        (if (< z-key (node-key x))
+          (loop (node-left x) x)
+          (loop (node-right x) x)))))
+  (set-node-parent! z y)
+  (set-node-left! z #f)
+  (set-node-right! z #f)
+  (if (not y)
+    (set-box! (int-tree-root I) z)
+    (begin
+      (set-node-color! z #f)
+      (if (< z-key (node-key y))
+        (set-node-left! y z)
+        (set-node-right! y z))
+      (insert-fixup I z))))
 
 (define (int-tree-search I k)
   (let loop ([n (unbox (int-tree-root I))])
@@ -113,6 +179,146 @@
    [(eq? o (node-left p))
     (set-node-left! p n)]
    [else
-    (set-ndoe-right! p n)])
+    (set-node-right! p n)])
   (node-free! o))
 
+;; -----------------------------------------------------------------------------
+;; --- private
+
+(define-syntax-rule (setColor z c) (and z (set-node-color! z c)))
+(define-syntax-rule (colorOf z) (and z (node-color z)))
+(define-syntax-rule (leftOf z) (and z (node-left z)))
+(define-syntax-rule (parentOf z) (and z (node-parent z)))
+(define-syntax-rule (rightOf z) (and z (node-right z)))
+
+(define (insert-fixup I z)
+  (define root (unbox (int-tree-root I)))
+  (let loop ([z z])
+    (when (and z (not (eq? z root)) (not (node-color (node-parent z))))
+      (if (eq? (node-parent z) (leftOf (parentOf (parentOf z))))
+        (let ([y (rightOf (parentOf (parentOf z)))])
+          (if (not (colorOf y))
+            (begin
+              (setColor (parentOf z) #f)
+              (setColor y #t)
+              (setColor (parentOf (parentOf z)) #f)
+              (loop (parentOf (parentOf z))))
+            (let ([z (if (eq? z (rightOf (parentOf z)))
+                       (begin (rotateLeft I (parentOf z))
+                              (parentOf z))
+                       z)])
+              (setColor (parentOf z) #t)
+              (setColor (parentOf (parentOf z)) #f)
+              (when (parentOf (parentOf z))
+                (rotateRight I (parentOf (parentOf z))))
+              (loop z))))
+        (let ([y (leftOf (parentOf (parentOf z)))])
+          (if (not (colorOf y))
+            (begin
+              (setColor (parentOf z) #t)
+              (setColor y #t)
+              (setColor (parentOf (parentOf z)) #f)
+              (loop (parentOf (parentOf z))))
+            (let ([z (if (eq? z (leftOf (parentOf z)))
+                       (begin (rotateRight I (parentOf z))
+                              (parentOf z))
+                       z)])
+              (setColor (parentOf z) #t)
+              (setColor (parentOf (parentOf z)) #f)
+              (when (parentOf (parentOf z))
+                (rotateLeft I (parentOf (parentOf z))))
+              (loop z)))))))
+  (set-node-color! root #t))
+
+(define (delete-fixup I x)
+  (define root (unbox (int-tree-root I)))
+  (let loop ([x x])
+    (when (and (not (eq? x root)) (colorOf x))
+      (if (eq? x (leftOf (parentOf x)))
+        (let* ([sib (rightOf (parentOf x))]
+               [sib (if (not (colorOf sib))
+                      (begin
+                        (setColor sib #t)
+                        (setColor (parentOf x) #f)
+                        (rotateLeft I (parentOf x))
+                        (rightOf (parentOf x)))
+                      sib)])
+          (if (and (colorOf (leftOf sib)) (colorOf (rightOf sib)))
+            (begin (setColor sib #f)
+                   (loop (parentOf x)))
+            (let ([sib (if (colorOf (rightOf sib))
+                         (begin
+                           (setColor (leftOf sib) #t)
+                           (setColor sib #f)
+                           (rotateRight I sib)
+                           (rightOf (parentOf x)))
+                         sib)])
+              (setColor sib (colorOf (parentOf x)))
+              (setColor (parentOf x) #t)
+              (setColor (rightOf sib) #t)
+              (rotateLeft I (parentOf x))
+              (loop root))))
+        (let* ([sib (leftOf (parentOf x))]
+               [sib (if (not (colorOf sib))
+                      (begin
+                        (setColor sib #t)
+                        (setColor (parentOf x) #f)
+                        (rotateRight I (parentOf x))
+                        (leftOf (parentOf x)))
+                      sib)])
+          (if (and (colorOf (rightOf sib)) (colorOf (leftOf sib)))
+            (begin (setColor sib #f)
+                   (loop (parentOf x)))
+            (let ([sib (if (colorOf (leftOf sib))
+                         (begin
+                           (setColor (rightOf sib) #t)
+                           (setColor sib #f)
+                           (rotateLeft I sib)
+                           (leftOf (parentOf x)))
+                         sib)])
+              (setColor sib (colorOf (parentOf x)))
+              (setColor (parentOf x) #t)
+              (setColor (leftOf sib) #t)
+              (rotateRight I (parentOf x))
+              (loop root)))))))
+  (setColor x #t))
+
+(define (rotateLeft I x)
+  (define y (rightOf x))
+  (set-node-right! x (node-left y))
+  (when (node-left y)
+    (set-node-parent! (node-left y) x))
+  (set-node-parent! y (node-parent x))
+  (cond
+   [(not (node-parent x))
+    (set-box! (int-tree-root I) y)]
+   [(eq? (leftOf (parentOf x)) x)
+    (set-node-left! (node-parent x) y)]
+   [else
+    (set-node-right! (node-parent x) y)])
+  (set-node-left! y x)
+  (set-node-parent! x y))
+
+(define (rotateRight I x)
+  (define y (node-left x))
+  (set-node-left! x (node-right y))
+  (when (node-right y)
+    (set-node-parent! (node-right y) x))
+  (set-node-parent! y (node-parent x))
+  (cond
+   [(not (node-parent x))
+    (set-box! (int-tree-root I) y)]
+   [(eq? x (node-right (node-parent x)))
+    (set-node-right! (node-parent x) y)]
+   [else
+    (set-node-left! (node-parent x) y)])
+  (set-node-right! y x)
+  (set-node-parent! x y))
+
+;; =============================================================================
+
+(module+ test
+  (require rackunit)
+
+
+)
